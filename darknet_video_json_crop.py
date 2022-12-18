839a9ad324ab6cfa66b39c7416b37774f890bd44
HZM111368024 <t111368024@ntut.org.tw>
2022-12-18 23:35:30 +0800

    modify nms and add full image detection

--- darknet_video_json_crop.py
+++ darknet_video_json_crop.py
@@ -12,6 +12,7 @@ import math
 import sys
 import ffmpeg
 
+
 def parser():
     parser = argparse.ArgumentParser(description="YOLO Object Detection")
     parser.add_argument("--input", type=str, default=0,
@@ -88,6 +89,19 @@ def convert2original(image, bbox, begin_x, begin_y):
     return bbox_converted
 
 
+def bbox2points(bbox):
+    """
+    From bounding box yolo format
+    to corner points cv2 rectangle
+    """
+    x, y, w, h = bbox
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+
 # 建立資料夾並儲存照片
 def storageImg(path, imgName, img):
     outputName = '.\\Yolov4_Output_Image\\' + path + '\\' + imgName + '.jpg'
@@ -110,6 +124,7 @@ def video_capture(frame_queue, darknet_image_queue):
     t = 0;
     crop_width = math.floor(width / 4)
     crop_height = math.floor(height / 4)
+    print(crop_width, crop_height)
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -118,6 +133,8 @@ def video_capture(frame_queue, darknet_image_queue):
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         frame_queue.put(frame)
+
+        # 切割畫面輸入
         for crop_x in range(0, math.floor(width - (crop_width * 4 / 10)), math.floor(crop_width * 0.6)):
             for crop_y in range(0, math.floor(height - (crop_height * 4 / 10)), math.floor(crop_height * 0.6)):
                 crop_img = frame_rgb[crop_y:crop_y + crop_height, crop_x:crop_x + crop_width]
@@ -128,24 +145,42 @@ def video_capture(frame_queue, darknet_image_queue):
                 crop_x_queue.put(crop_x)
                 crop_y_queue.put(crop_y)
                 darknet_image_queue.put(img_for_detect)
-        t = t + 1
+
+        # 完整圖片輸入
+        crop_img_resized = cv2.resize(frame_rgb, (darknet_width, darknet_height),
+                                      interpolation=cv2.INTER_LINEAR)
+        img_for_detect = darknet.make_image(darknet_width, darknet_height, 3)
+        darknet.copy_image_from_bytes(img_for_detect, crop_img_resized.tobytes())
+        crop_x_queue.put(0)
+        crop_y_queue.put(0)
+        darknet_image_queue.put(img_for_detect)
+
+        print("video_capture times : " + str(t))
     cap.release()
 
 
 def inference(darknet_image_queue, detections_queue, fps_queue):
-    t = 1;
     while cap.isOpened():
         prev_time = time.time()
+        # 切割畫面偵測
         for i in range(0, 6, 1):
             for j in range(0, 6, 1):
                 darknet_image = darknet_image_queue.get()
                 detections = darknet.detect_image(network, class_names, darknet_image, thresh=args.thresh,
-                                                  hier_thresh=.5, nms=.1)
+                                                  hier_thresh=.5, nms=0)
                 detections_queue.put(detections)
                 darknet.print_detections(detections, args.ext_output)
                 darknet.free_image(darknet_image)
+        # 完整畫面偵測
+        darknet_image = darknet_image_queue.get()
+        detections = darknet.detect_image(network, class_names, darknet_image, thresh=args.thresh,
+                                          hier_thresh=.5, nms=0)
+        detections_queue.put(detections)
+        darknet.print_detections(detections, args.ext_output)
+        darknet.free_image(darknet_image)
+
         fps = int(1 / (time.time() - prev_time))
-        print("Fps" + str(fps))
+        print("video_capture times : " + str(inference_time))
         fps_queue.put(fps)
     cap.release()
 
@@ -153,7 +188,6 @@ def inference(darknet_image_queue, detections_queue, fps_queue):
 def drawing(frame_queue, detections_queue, fps_queue):
     random.seed(3)  # deterministic bbox colors
     # video = set_saved_video(cap, args.out_filename, (width, height))
-    t = 1;
     # 6FPS
 
     time = 0
@@ -167,9 +201,12 @@ def drawing(frame_queue, detections_queue, fps_queue):
         fps = fps_queue.get()
         all_confidence = []
         all_boxes = []
+        all_boxes_nms = []
         all_label = []
         detections_adjusted = []
         if frame is not None:
+
+            # 分割畫面繪製
             for i in range(0, 6, 1):
                 for j in range(0, 6, 1):
                     detections = detections_queue.get()
@@ -178,11 +215,28 @@ def drawing(frame_queue, detections_queue, fps_queue):
                     for label, confidence, bbox in detections:
                         if label == "Pistol":
                             bbox_adjusted = convert2original(frame, bbox, crop_x, crop_y)
+                            bbox_nms = bbox2points(convert2original(frame, bbox, crop_x, crop_y))
                             if bbox_adjusted[2] < crop_width / 2 and bbox_adjusted[3] < crop_height / 2:
+                                all_boxes_nms.append(bbox_nms)
                                 all_confidence.append(float(confidence))
                                 all_boxes.append(bbox_adjusted)
                                 all_label.append(label)
-            idxs = cv2.dnn.NMSBoxes(all_boxes, all_confidence, 0.5, 0.2)
+
+            # 完整畫面繪製
+            detections = detections_queue.get()
+            crop_x = crop_x_queue.get()
+            crop_y = crop_y_queue.get()
+            for label, confidence, bbox in detections:
+                if label == "Pistol":
+                    bbox_adjusted = convert2original(frame, bbox, crop_x, crop_y)
+                    bbox_nms = bbox2points(convert2original(frame, bbox, crop_x, crop_y))
+                    if bbox_adjusted[2] < crop_width / 2 and bbox_adjusted[3] < crop_height / 2:
+                        all_boxes_nms.append(bbox_nms)
+                        all_confidence.append(float(confidence))
+                        all_boxes.append(bbox_adjusted)
+                        all_label.append(label)
+
+            idxs = cv2.dnn.NMSBoxes(all_boxes_nms, all_confidence, 50, 0.1)
             if len(idxs) > 0:
                 for i in idxs.flatten():
                     (x, y) = (all_boxes[i][0], all_boxes[i][1])
@@ -191,9 +245,9 @@ def drawing(frame_queue, detections_queue, fps_queue):
             image = darknet.draw_boxes(detections_adjusted, frame, class_colors)
             probe = ffmpeg.probe(args.input)
             video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-            video_trueFPS = int(video_stream['r_frame_rate'].split('/')[0]) / int(video_stream['r_frame_rate'].split('/')[1])
-            print((round(video_trueFPS/6)))
-            if timeCount % (round(video_trueFPS/6)) == 0:
+            video_trueFPS = int(video_stream['r_frame_rate'].split('/')[0]) / int(
+                video_stream['r_frame_rate'].split('/')[1])
+            if timeCount % (round(video_trueFPS / 6)) == 0:
                 imgName = targetName + '#t=' + str(time + timestamp_fps[storetime % 6])
                 # 儲存圖片
                 storageImg(targetName, imgName, image)
@@ -207,8 +261,6 @@ def drawing(frame_queue, detections_queue, fps_queue):
                 cv2.imshow('Inference', image)
             if cv2.waitKey(fps) == 27:
                 break
-
-        t = t + 1
         # print(str(time + timestamp_fps[timeCount]))
         timeCount = timeCount + 1
 
@@ -264,7 +316,7 @@ def output_json_file():
 
 
 if __name__ == '__main__':
-    # 用法: python x64/darknet_video_json_crop.py --input C:\Users\WIN\Desktop\碩士\test.mp4
+    # 用法: python x64/darknet_video_json_crop.py --dont_show --input C:\Users\WIN\Desktop\碩士\test.mp4
     args = parser()
     input_path = str2int(args.input)
     substrName = input_path.rfind('\\')
@@ -276,11 +328,11 @@ if __name__ == '__main__':
     regions = []
 
     frame_queue = Queue()
-    darknet_image_queue = Queue(maxsize=36)
-    detections_queue = Queue(maxsize=36)
+    darknet_image_queue = Queue(maxsize=37)
+    detections_queue = Queue(maxsize=37)
     fps_queue = Queue(maxsize=1)
-    crop_x_queue = Queue(maxsize=36)
-    crop_y_queue = Queue(maxsize=36)
+    crop_x_queue = Queue(maxsize=37)
+    crop_y_queue = Queue(maxsize=37)
 
     check_arguments_errors(args)
     network, class_names, class_colors = darknet.load_network(
